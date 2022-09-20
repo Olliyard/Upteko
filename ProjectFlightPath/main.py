0f6a782539a5813f5942001cdf0c19ed0ecf313a
yarsel <yarslan02@live.dk>
2022-09-20 15:28:55 +0200

    Co-authored-by: Oliver Malling <revilo624@gmail.com>

--- ProjectFlightPath/main.py
+++ ProjectFlightPath/main.py
@@ -1,1 +1,13 @@
-#import
+# import
+#%%
+#%matplotlib notebook
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+ax = plt.axes(projection = '3d')
+fig.show()
+
+# %%

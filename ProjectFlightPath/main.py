f69bdf4f1dc5f2d79cb78fa8820469424b25b8dc
yarsel <yarslan02@live.dk>
2022-09-22 14:20:39 +0200

    Co-authored-by: Oliver Malling <revilo624@gmail.com>

--- ProjectFlightPath/main.py
+++ ProjectFlightPath/main.py
@@ -4,19 +4,50 @@ import mpl_toolkits.mplot3d.art3d as art3d
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Circle
+import statistics as st
+import csv
 
-def update_plot(plot, new_data):
-    plot.set_xdata(np.append(plot.get_xdata(), new_data))
-    plot.set_ydata(np.append(plot.get_ydata(), new_data))
-    plt.draw()
-    
+
+def printcsv():
+    """
+    Convert a csv file to x, y and z coordinates.
+    """
+    x = []
+    y = []
+    z = []
+    with open('drone_local_position.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            x.append(row[0])
+            y.append(row[1])
+            z.append(row[2])
+            #print(f'x: {row[0]}, y: {row[1]}, z: {row[2]}')
+            line_count += 1
+        x = list(map(float, x))
+        y = list(map(float, y))
+        z = list(map(float, z))
+        return x, y, z
+
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    """
+    Map the lower and upper values of dataset to a specified boundary and adjust accordingly.
+    x: data list
+    in_min: the lowest value of the x dataset
+    in_max: the highest value of the x dataset
+    out_min: the user defined lower bound
+    out_max: the user defined upper bound
+    """
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
 """
 Class: Cylinder
 Description: Initialize cylinder object for print
 Param: radius - the radius of the cylinder
 Param: height - the height of the cylinder
 Param: color - the color of the cylinder
-Return: None
 """
 class Cylinder:
     def __init__(self, radius, height, color):
@@ -28,7 +59,7 @@ class Cylinder:
         self.resolution = 100
         self.elevation = 0
     
-    def print_fig(self):    #Is it better to just create a simpler transparent figure?
+    def print_fig(self):
         #simpler implementation
         z = np.linspace(0, self.height, self.resolution)    #Create a figure with x height around z-axis
         theta = np.linspace(0, 2*np.pi, self.resolution)    #Create a theta value to mesh with 
@@ -52,93 +83,72 @@ class Cylinder:
 
 class Drone(Cylinder):
     def __init__(self, cylinder):
-        self.radius = cylinder.radius + 1
-        self.height = cylinder.height + 1
+        self.radius = cylinder.radius
+        self.height = cylinder.height
         self.resolution = 1
+        self.max_upper = self.radius+2
+        self.min_upper = self.radius+1
+        self.max_lower = -self.radius-2
+        self.min_lower = -self.radius-1
 
+    def map_coords(self, coordinate):
+        """
+        map the coordinates
         
-    def flight_print(self):
-        #Flight up (x: 0, y: -3, z: 0-6)
-        self.x_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.y_coord = [-3, -3, -3, -3, -3, -3, -3]
-        self.z_coord = list(range(0, self.height+1, self.resolution))
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        ax.plot(self.x_coord, self.y_coord, self.z_coord, color='r')
-        
-        #Flight over (x: 0, y: (-3)-3, z: 6)
-        self.x_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.y_coord = list(range(-(self.radius), self.radius+1, self.resolution))
-        self.z_coord = [6, 6, 6, 6, 6, 6, 6]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        
-        #Flight down (x: 0, y: 3, z: 6-0)
-        self.x_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.y_coord = [3, 3, 3, 3, 3, 3, 3]
-        self.z_coord = list(range(self.height, -1, -self.resolution))
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        #Flight around (x: 0-3, y: 3, z: 0)
-        self.x_coord = list(range(0, self.radius+1, self.resolution))
-        self.y_coord = [3, 3, 3, 3]
-        self.z_coord = [0, 0, 0, 0]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
+        coordinate: list of coordinates
+        """
+        mean_coordinate = st.mean(coordinate)
+        new_coordinate = []
+        upper_coordinate = []
+        lower_coordinate =[]
 
-        #Flight around contd. (x: 3, y: 3-0, z: 0)
-        self.x_coord = [3, 3, 3, 3]
-        self.y_coord = list(range(self.radius, -1, -self.resolution))
-        self.z_coord = [0, 0, 0, 0]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        
-        #Flight up
-        self.x_coord = [3, 3, 3, 3, 3, 3, 3]
-        self.y_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.z_coord = list(range(0, self.height+1, self.resolution))
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        
-        #Flight over (x: 3-(-3), y: 0, z: 6)
-        #self.x_coord = [3, 3, 3, 3, 3, 3, 3]
-        self.x_coord = list(range(self.radius, -(self.radius+1), -self.resolution))
-        self.y_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.z_coord = [6, 6, 6, 6, 6, 6, 6]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        
-        #Flight down
-        self.x_coord = [-3, -3, -3, -3, -3, -3, -3]
-        self.y_coord = [0, 0, 0, 0, 0, 0, 0]
-        self.z_coord = list(range(self.height, -1, -self.resolution))
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
-        #Flight around
-        self.x_coord = [-3, -3, -3, -3]
-        self.y_coord = list(range(0, -(self.radius+1), -self.resolution))
-        self.z_coord = [0, 0, 0, 0]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
+        for data in coordinate:
+            if(data >= mean_coordinate):
+                upper_coordinate.append(data)
+            else:
+                lower_coordinate.append(data)
+    
+        max_upper_coord = max(upper_coordinate)
+        min_upper_coord = min(upper_coordinate)
+        max_lower_coord = max(lower_coordinate)
+        min_lower_coord = min(lower_coordinate)
 
+        for data in coordinate:
+            if(data >= mean_coordinate):
+                new_coordinate.append(map_range(data, min_upper_coord, max_upper_coord, self.min_upper, self.max_upper))
+            elif (data < mean_coordinate):
+                new_coordinate.append(map_range(data, min_lower_coord, max_lower_coord, self.min_lower, self.max_lower))
         
-        #Flight around contd.
-        self.x_coord = list(range(-(self.radius), 1, self.resolution))
-        self.y_coord = [-3, -3, -3, -3]
-        self.z_coord = [0, 0, 0, 0]
-        ax.scatter(self.x_coord, self.y_coord, self.z_coord)
-        
+        return new_coordinate
         
     def update_coordinates(x_coord, y_coord, z_coord):
         pass
-        
         
 
 
 fig=plt.figure()
 ax = Axes3D(fig)
 
-cylinder1 = Cylinder(2, 5, 'b')
+#Create cylinder object with radius 2, height 5 and color 'blue'
+cylinder1 = Cylinder(3, 20, 'b')
+
+#Create drone object which inherits the beforementioned cylinder object values.
 drone1 = Drone(cylinder1)
+
+#Print the cylinder object
 cylinder1.print_fig()
-drone1.flight_print()
+
+x, y, z  =printcsv()
+ax.scatter(x,y,z)
+"""
+new_x = drone1.map_coords(x)
+new_y = drone1.map_coords(y)
+ax.scatter(new_x, new_y, z)
+"""
+
+#Print the drone flight path
+#drone1.flight_print()
+
 ax.set_xlabel('x-axis')
 ax.set_ylabel('y-axis')
 ax.set_zlabel('z-axis')
@@ -146,6 +156,4 @@ plt.show()
 
 #Thinking of implementing an update loop which updates the drones flight path live every x seconds. 
 
-
 # %%
-

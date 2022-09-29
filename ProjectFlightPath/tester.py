cce33e9667cafbc7ba65bcefd76786db7d2e1aba
yarsel <yarslan02@live.dk>
2022-09-29 09:49:39 +0200

    Co-authored-by: Oliver Malling <revilo624@gmail.com>

--- ProjectFlightPath/tester.py
+++ ProjectFlightPath/tester.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # import csv
 
 # #Get .csv file and put it into x,y,z list
@@ -27,33 +26,34 @@
 # time = printcsv()
 # print("Time: ", time[1], "\r\n")
 
+# import csv
+# fields = ["time", "header", "pose"]
+# with open('drone_local_position_unformated.csv') as csvfile:
+#     reader = csv.DictReader((line.replace(',"', '&') for line in csvfile), fieldnames=fields, delimiter='&', quotechar='&', doublequote=False)
+#     line_count = 0
+#     for row in reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             print(row['pose'])
+#             line_count += 1
+
+# %%
 import csv
-fields = ["time", "header", "pose"]
-with open('drone_local_position_unformated.csv') as csvfile:
-    reader = csv.DictReader((line.replace(',"', '&') for line in csvfile), fieldnames=fields, delimiter='&', quotechar='&', doublequote=False)
-    line_count = 0
-    for row in reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(row['pose'])
-            line_count += 1
-=======
-f = open('drone_local_position_unformated.csv', 'r')
-data = f.read()
-
-line_count = 0
-for row in data:
-    if line_count == 0:
-        print(f'Column names are {",".join(row)}')
-        line_count += 1
-    else:
-        print(f'\t{row[0]}.')
-        break
-
-for line in data:
-    print(data[29:40])
-
-# drone_local_position_unformated.csv
->>>>>>> 938b7a3 (Co-authored-by: Oliver Malling <revilo624@gmail.com>)
+import json
+with open("drone_local_position_unformated.csv", 'r') as file:
+    headers = ['time', 'header', 'pose']
+    csvreader = csv.DictReader(file, delimiter=',',  fieldnames=headers)
+    for i, row in enumerate(csvreader):
+        if i == 0:
+            continue 
+        if i > 1:
+            break 
+        
+        print(row)
+        print(row['pose'])
+        a = row['pose'].replace("'", "\"")
+        convertedDict = json.loads(a)
+        print(convertedDict)
+        print(convertedDict['position'])
